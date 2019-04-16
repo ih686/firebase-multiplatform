2578e7edb1da74360f7ca10acde5f3bd6bfd4697
sadevelopment <marcus@squareapple.co.uk>
2019-04-16 11:40:46 +0100

    Implement Auth

--- src/jsMain/kotlin/multiplatform/com/google/firebase/auth/auth.kt
+++ src/jsMain/kotlin/multiplatform/com/google/firebase/auth/auth.kt
@@ -1,40 +1,66 @@
 package multiplatform.com.google.firebase.auth
 
+import kotlinx.coroutines.await
+import kotlin.js.Promise
+
 @JsModule("firebase")
 external val firebase: dynamic
 @JsModule("firebase/auth")
 external val auth: dynamic
 
-//actual class FirebaseAuth {
-//
-//    private val instance = firebase.auth()
-//
-//    actual companion object {
-//        private val instance = FirebaseAuth()
-//        actual fun getInstance() = instance
-//    }
-//
-//    actual fun signInWithCustomToken(token: String) = instance.signInWithCustomToken(token).asDeferred()
-//
-//    actual val currentUser: FirebaseUser?
-//        get() = TODO("not implemented")
-//
-//    actual fun signOut() {}
-//
-//    actual interface AuthStateListener {
-//        actual fun onAuthStateChanged(auth: FirebaseAuth)
-//    }
-//
-//    actual fun addAuthStateListener(listener: AuthStateListener) {}
-//
-//}
-//
-//actual class AuthResult {
-//    actual val user: FirebaseUser
-//        get() = TODO("not implemented")
-//}
-//
-//actual class FirebaseUser {
-//    actual val uid: String
-//        get() = TODO("not implemented")
-//}
+actual fun getFirebaseAuth() = firebase.auth()
+
+actual interface AuthStateListener {
+    actual fun onAuthStateChanged(auth: FirebaseAuth)
+}
+
+@JsModule("firebase/auth")
+open external class Auth {
+    val currentUser: FirebaseUser?
+}
+
+actual typealias FirebaseAuth = Auth
+
+actual val FirebaseAuth.currentUser: FirebaseUser?
+    get() = currentUser?.let{ it }
+
+@JsModule("firebase/auth")
+open external class UserCredential {
+    val user: User
+}
+
+actual interface AuthResult {
+    val user: FirebaseUser
+}
+
+actual val AuthResult.user: FirebaseUser
+    get() = user
+
+@JsModule("firebase/auth")
+open external class User {
+    val uid: String
+    val isAnonymous: Boolean
+}
+
+actual abstract class FirebaseUser : User()
+
+actual val FirebaseUser.uid: String
+    get() = uid
+
+actual suspend fun FirebaseAuth.awaitSignInWithCustomToken(token: String) = (firebase.auth().signInWithCustomToken(token) as Promise<AuthResult>).await()
+
+actual suspend fun FirebaseAuth.awaitSignInAnonymously() = (firebase.auth().signInAnonymously() as Promise<AuthResult>).await()
+
+actual val FirebaseUser.isAnonymous: Boolean
+    get() = isAnonymous
+
+actual suspend fun FirebaseUser.awaitDelete() = firebase.auth().currentUser?.delete()
+
+actual suspend fun FirebaseUser.awaitReload() = firebase.auth().currentUser?.reload()
+
+actual fun FirebaseAuth.addAuthStateListener(listener: AuthStateListener)  = firebase.auth().onAuthStateChanged(listener)
+
+actual fun FirebaseAuth.removeAuthStateListener(listener: AuthStateListener) {
+}
+
+actual fun FirebaseAuth.signOut() = firebase.auth().signOut()
