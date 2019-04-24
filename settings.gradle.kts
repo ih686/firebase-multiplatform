9487538d122ebeee41c5b1adebe4fb3d2a94fea9
Nick Williams <nbransby@gmail.com>
2019-04-24 13:31:28 +0100

    add android back in

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,18 @@
 enableFeaturePreview("GRADLE_METADATA")
 
 rootProject.name = "firebase"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        jcenter()
+        google()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.android.library") {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+        }
+    }
+}
